28eead6bc6565a58abfd8da26f2a1879650930e0
brunocappelli <brunocappelli@outlook.com>
2015-11-05 16:55:03 -0200

    hn script almost done

--- hn.py
+++ hn.py
@@ -1,3 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: iso-8859-15 -*-
 #this will get the top stories from HackerNews (news.ycombinator.com)
+
+from hn import HN, Story
+
+hn = HN()
+
+# print the first 2 pages of newest stories
+for story in hn.get_stories(story_type='newest', limit=60):
+        print(story.rank, story.title)
